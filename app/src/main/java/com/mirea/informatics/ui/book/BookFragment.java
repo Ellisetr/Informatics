1a51f39327e297f6f1b87adfd37091c5c47bdc24
Ellisetr <skater46004@gmail.com>
2020-03-30 00:57:03 +0300

    Initial commit

---
+++ app/src/main/java/com/mirea/informatics/ui/book/BookFragment.java
@@ -0,0 +1,40 @@
+package com.mirea.informatics.ui.book;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebViewFragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.mirea.informatics.R;
+
+public class BookFragment extends Fragment {
+
+    static WebView myWebView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_book, container, false);
+        return root;
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        myWebView = (WebView) getActivity().findViewById(R.id.bookreader);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://www.google.com");
+    }
+}
